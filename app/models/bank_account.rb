5f763f671e4c03e4111882c8e302364715d115cf
rociofdm <rocio.fdez.diaz.meco@gmail.com>
2018-02-03 20:11:22 +0100

    fix relationship between bank account and payment

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -2,4 +2,6 @@ class BankAccount < ApplicationRecord
   belongs_to :bank
   validates_presence_of :balance
   validates_presence_of :iban
+  has_many :origin_payments, :class_name => "Payment", :foreign_key => "origin_id"
+  has_many :destination_payments, :class_name => "Payment", :foreign_key => "destination_id"
 end

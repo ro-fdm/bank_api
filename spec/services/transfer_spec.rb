32748001f10e254f2d50f3f7eb03259ef84ac6fe
Rocío Fernandez <rocio@besepa.com>
2018-02-05 13:21:10 +0100

    fix bug 500 comision is 5 euros

--- spec/services/transfer_spec.rb
+++ spec/services/transfer_spec.rb
@@ -3,18 +3,18 @@ require "rails_helper"
 describe Transfer do
   let(:user) { create(:user) }
   let(:bank) { FactoryBot.create(:bank) }
-  let(:origin_account){ FactoryBot.create(:bank_account, bank: bank, balance: 15) }
-  let(:destination_account) { FactoryBot.create(:bank_account, bank: bank, balance: 10) }
+  let(:origin_account){ FactoryBot.create(:bank_account, bank: bank, balance: 1500) }
+  let(:destination_account) { FactoryBot.create(:bank_account, bank: bank, balance: 1000) }
   let(:payment_intra_bank){ FactoryBot.create(:payment,
-                                              amount: 1,
+                                              amount: 100,
                                               origin: origin_account,
                                               destination: destination_account,
                                               kind: "transfer" )}
 
   let(:bank2) { FactoryBot.create(:bank) }
-  let(:destination_account_different_bank){ FactoryBot.create(:bank_account, bank: bank2, balance: 10) }
+  let(:destination_account_different_bank){ FactoryBot.create(:bank_account, bank: bank2, balance: 1000) }
   let(:payment_inter_bank){ FactoryBot.create(:payment,
-                                              amount: 1,
+                                              amount: 100,
                                               origin: origin_account,
                                               destination: destination_account_different_bank,
                                               kind: "transfer" )}
@@ -25,8 +25,8 @@ describe Transfer do
 
     expect(result).to be_truthy
     expect(payment_intra_bank.comision).to eq(0)
-    expect(payment_intra_bank.origin.balance).to eq(14)
-    expect(payment_intra_bank.destination.balance).to eq(11)
+    expect(payment_intra_bank.origin.balance).to eq(1400)
+    expect(payment_intra_bank.destination.balance).to eq(1100)
   end
 
   it "transfer inter bank" do
@@ -34,9 +34,9 @@ describe Transfer do
     result = transfer.new(payment_inter_bank).run!
 
     expect(result).to be_truthy
-    expect(payment_inter_bank.comision).to eq(5)
-    expect(payment_inter_bank.origin.balance).to eq(9)
-    expect(payment_inter_bank.destination.balance).to eq(11)
+    expect(payment_inter_bank.comision).to eq(500)
+    expect(payment_inter_bank.origin.balance).to eq(900)
+    expect(payment_inter_bank.destination.balance).to eq(1100)
   end
 
   it "transfer inter bank fails by exceed limit" do
@@ -50,8 +50,8 @@ describe Transfer do
 
     expect(result).to be_falsey
     expect(payment.comision).to eq(nil)
-    expect(payment.origin.balance).to eq(15)
-    expect(payment.destination.balance).to eq(10)
+    expect(payment.origin.balance).to eq(1500)
+    expect(payment.destination.balance).to eq(1000)
   end
 
 end
